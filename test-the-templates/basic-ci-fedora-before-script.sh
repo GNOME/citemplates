1d54b90dc231dd502d8ada57ebd96a9bfa5b1f2b
Jordan Petridis <jpetridis@gnome.org>
2025-06-06 06:41:06 +0300

    gnomeos-basic-ci: Add a clang build job variant
    Same as the plain build job with clang

--- test-the-templates/basic-ci-fedora-before-script.sh
+++ test-the-templates/basic-ci-fedora-before-script.sh
@@ -3,5 +3,5 @@
 set -eux
 
 dnf builddep -y gnome-font-viewer
-dnf install -y git meson dbus-run-session mutter
+dnf install -y git meson dbus-run-session mutter gcc g++ clang clang++
 git clone --depth=1 https://gitlab.gnome.org/gnome/gnome-font-viewer.git
--- test-the-templates/basic-ci-ubuntu-before-script.sh
+++ test-the-templates/basic-ci-ubuntu-before-script.sh
@@ -4,5 +4,5 @@ set -eux
 
 sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
 apt update -yq && apt build-dep -y gnome-font-viewer
-apt install -y git mutter meson
+apt install -y git mutter meson gcc clang
 git clone --depth=1 https://gitlab.gnome.org/gnome/gnome-font-viewer.git
