10e1d44ac3054c01ae2e6108ba351e15b3cc0482
Jordan Petridis <jpetridis@gnome.org>
2026-09-15 20:23:38 +0300

    flatpak-job: Remove duplicate command
    Looks like a rebase snafu

--- runtime-images/flatpak-job/flatpak-job.sh
+++ runtime-images/flatpak-job/flatpak-job.sh
@@ -134,9 +134,6 @@ determine_cache_image () {
 # Make sure there is no leftover for whatever reason
 rm -rf ./flatpak_app ./.flatpak-builder/build
 
-# Make sure there is no leftover for whatever reason
-rm -rf ./flatpak_app ./.flatpak-builder/build
-
 bundle="$(get_bundle_name)"
 readonly bundle
 echo "Bundle filename: ${bundle}"
