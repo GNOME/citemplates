3cc51931b9fb1e89d8a572312cb29c2ecf60bbc2
Jordan Petridis <jpetridis@gnome.org>
2026-09-19 00:41:46 +0300

    flatpak-job: Fix repo.tar archived artifact
    tar when given an absolute path, it will happily craete a directory with
    the structure of the path, even when we really only want the last directory.
    
    So when we switched from ./repo to $CI_PROJECT_DIR/repo, the archive would
    now contain /build/projectname/repo and this broke the nightly publishing
    as it expects a single "repo" directory.
    
    Use --directory and its parent directory in order to do this properly,
    and also expand the test job to make sure.

--- runtime-images/flatpak-job/flatpak-job.sh
+++ runtime-images/flatpak-job/flatpak-job.sh
@@ -211,7 +211,7 @@ flatpak build-bundle \
 sha256sum "${project_dir}/${bundle}" > "${project_dir}/${bundle}.sha256sum"
 
 # Tar the repo for export in the artifacts, this gets consumed by the publish_nightly jobs
-tar cf "$project_dir/repo.tar" "${application_repo}"
+tar --create --file "$project_dir/repo.tar" --directory "${project_dir}" repo
 
 # Export the documentation if it exist
 docs_path="${application_directory}/files/share/doc/"
