76d327888e66e1c0298e9db039e0d2d7d9905534
Jordan Petridis <jpetridis@gnome.org>
2026-09-18 12:46:30 +0300

    flatpak-job: Fix cache upload
    This broke in 44890e5
    
    The pull script was changed, but forgot to also update the cache publish

--- runtime-images/flatpak-job/upload-cache.sh
+++ runtime-images/flatpak-job/upload-cache.sh
@@ -6,7 +6,6 @@
 set -e -o pipefail
 set +x
 
-# FIXME: this relies on the registry in the other script
 if [[ -n "${NIGHTLY_CACHE_ORAS_TOKEN_FILE:-}" ]] && [[ "${PUBLISH_CACHE:-0}" == "1" ]] && [[ "${CI_COMMIT_BRANCH}" == "${CI_DEFAULT_BRANCH}" ]]; then
     echo "Pruning ccache objects older than 90d..."
     ccache --show-stats --dir ${state_dir}/ccache/ --evict-older-than 90d
@@ -16,7 +15,7 @@ if [[ -n "${NIGHTLY_CACHE_ORAS_TOKEN_FILE:-}" ]] && [[ "${PUBLISH_CACHE:-0}" == 
     cat $NIGHTLY_CACHE_ORAS_TOKEN_FILE | oras login -u "${NIGHTLY_CACHE_ORAS_USER}" --password-stdin ${_ORAS_CACHE_REGISTRY} || true
     tar --create --xattrs --zstd --file "builder.tar.zstd" --exclude ${state_dir}/build --exclude ${state_dir}/rofiles ${state_dir}/ || true
     du -hs "builder.tar.zstd" || true
-    oras push $registry "builder.tar.zstd" || true
+    oras push "$ORAS_CACHE_IMAGE" "builder.tar.zstd" || true
     oras logout ${_ORAS_CACHE_REGISTRY} || true
 fi
 
