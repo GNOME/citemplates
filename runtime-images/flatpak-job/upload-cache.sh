9671f444e8d4686694a11a1c38b1ecb8714eb083
Jordan Petridis <jpetridis@gnome.org>
2026-09-20 12:46:58 +0300

    flatpak-job: Expire the cache images after 30d

--- runtime-images/flatpak-job/upload-cache.sh
+++ runtime-images/flatpak-job/upload-cache.sh
@@ -15,7 +15,7 @@ if [[ -n "${NIGHTLY_CACHE_ORAS_TOKEN_FILE:-}" ]] && [[ "${PUBLISH_CACHE:-0}" == 
     cat $NIGHTLY_CACHE_ORAS_TOKEN_FILE | oras login -u "${NIGHTLY_CACHE_ORAS_USER}" --password-stdin ${_ORAS_CACHE_REGISTRY} || true
     tar --create --xattrs --zstd --file "builder.tar.zstd" --exclude ${state_dir}/build --exclude ${state_dir}/rofiles ${state_dir}/ || true
     du -hs "builder.tar.zstd" || true
-    oras push "$ORAS_CACHE_IMAGE" "builder.tar.zstd" || true
+    oras push --annotation "quay.expires-after=30d" "$ORAS_CACHE_IMAGE" "builder.tar.zstd" || true
     oras logout ${_ORAS_CACHE_REGISTRY} || true
 fi
 
